4a6f189d19ddf48e15665b9dc74d2a7c1b88d12a
Alexromeu <alexxromeu@gmail.com>
2026-02-13 19:54:34 -0600

    neural network initialized

--- neural_network.c
+++ neural_network.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define INPUT 50 
-#define HIDDEN 20 
-#define OUTPUT 10
+#define MAX_WEIGHTS 10
+#define MAX_NEURONS_PER_LAY 10
+#define TOTAL_LAYERS 10 
 
 //OJO: weights are associated with connections
 typedef struct __neuron_t {
     float value;
     float bias;
-    float *weights;
+    float weights[MAX_WEIGHTS];
 } neuron_t;
 
 typedef struct __layer_t {
@@ -23,14 +23,45 @@ typedef struct __neural_network_t {
 } neural_network_t;
 
 void initialize_network(neural_network_t *network) {
-    int total_layers = network->num_layers;
-    layer_t *layers = malloc(total_layers * sizeof(layer_t));
-    
+    int count = 0;
+    network->layers = malloc(network->num_layers * sizeof(layer_t));
+
+    for (int i = 0; i < network->num_layers; i++) {
+        layer_t *layer = &network->layers[i];
+        layer->count = MAX_NEURONS_PER_LAY;
+        layer->neurons = malloc(layer->count * sizeof(neuron_t));
+
+        for (int j = 0; j < layer->count; j++) {
+            neuron_t *n = &layer->neurons[j];
+
+            n->value = 0;
+            n->bias = 0;
+
+            for (int k = 0; k < MAX_WEIGHTS; k++) {
+                n->weights[k] = 2;
+            }
+        }
+    }
+   
+    for (int i = 0; i < network->num_layers; i++) { 
+        for (int j = 0; j < network->layers[i].count; j++) {    
+            for (int k = 0; k < MAX_WEIGHTS; k++) {
+                count++;
+                printf("%f\n", network->layers[i].neurons[j].weights[k]);
+            }    
+        }
+    }
+printf("neurons count-> %d\n", count);
+    //network so far initialize with #2 to test
+    //next we need to add a random nums function to set the values random
+    // and set a function to feed input to first layer
 }
 
 
 int main(void) {
-
+    neural_network_t network;
+    network.num_layers = TOTAL_LAYERS;
+    initialize_network(&network);
 
     return 0;
 }
