032135dadc5a41ecb220b82c768561d46a66e799
Alexromeu <alexxromeu@gmail.com>
2026-02-15 21:07:29 -0600

    3.0

--- neural_network.c
+++ neural_network.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
 
 #define MAX_WEIGHTS 20
 #define MAX_NEURONS_PER_LAY 20
-#define TOTAL_LAYERS 10 
+#define TOTAL_LAYERS 3 
 #define INPUT_LAYER_MAXVAL 10
 
 //OJO: weights are associated with connections
@@ -43,28 +45,54 @@ void initialize_network(neural_network_t *network) {
             n->bias = 0;
 
             for (int k = 0; k < MAX_WEIGHTS; k++) {
-                n->weights[k] = 2;
+                n->weights[k] = 2; //NEED RANDOM FUNCTION FOR THIS randnum(seed)
             }
         }
-    }
-
-    float feed_neuron(neuron_t *neuron, input_t *input, size_t input_count) {
-        //while ()
     }
    
     for (int i = 0; i < network->num_layers; i++) { 
         for (int j = 0; j < network->layers[i].count; j++) {    
             for (int k = 0; k < MAX_WEIGHTS; k++) {
                 count++;
-                printf("%f\n", network->layers[i].neurons[j].weights[k]);
+                //printf("%f\n", network->layers[i].neurons[j].weights[k]);
             }    
         }
     }
 
-    printf("neurons count-> %d\n", count);
     //network so far initialize with #2 to test
     //next we need to add a random nums function to set the values random
-    // and set a function to feed input to first layer
+}
+
+float feed_neuron(neuron_t *neuron, float *input_layer, int input_count) {
+    int i = 0;
+    float result = 0.0f;
+
+    while (i < input_count) {
+        result += neuron->weights[i] * input_layer[i]; 
+        i++;
+    }
+
+    return result + neuron->bias; //res to pass to next layer neuronS
+}
+
+float sigmoid(float x) {
+    return 1.0f / (1.0f + expf(-x));
+}
+
+//this process layer 1 and pass data to layer 2 editing layer 2
+//same process will repeat in layer 3 inheriting data from 2
+// then need to deal with the output
+void layer_processing(layer_t *in_layer, layer_t *out_layer) {
+    float input_values[MAX_NEURONS_PER_LAY];
+    for (int i = 0; i < in_layer->count; i++) {
+        input_values[i] = in_layer->neurons[i].value;
+    }
+
+    for (int lo = 0; lo < out_layer->count; lo++) {
+        float raw_sum = feed_neuron(&out_layer->neurons[lo], input_values, in_layer->count);
+        out_layer->neurons[lo].value = sigmoid(raw_sum);
+    }
+
 }
 
 
@@ -72,6 +100,15 @@ int main(void) {
     neural_network_t network;
     network.num_layers = TOTAL_LAYERS;
     initialize_network(&network);
+
+    layer_t *layer1 = &(network.layers[0]);
+    layer_t *layer2 = &(network.layers[1]);
+
+    layer_processing(layer1, layer2);
+
+    for (int i = 0; i < layer2->count; i++) {
+        printf("%f\n", layer2->neurons->value);
+    }
 
     return 0;
 }
